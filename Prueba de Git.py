97ea80bc0c25cb78a4ded49e837a35203b973366
SrChaoz <mateorubencho@gmail.com>
2024-10-24 11:41:06 -0500

    primer commit

---
+++ Prueba de Git.py
@@ -0,0 +1,22 @@
+print("Hola mundo")
+a=1
+b=3
+c=a+b
+print(c)
+
+def sumadenumero(a,b):
+    a=int(input("Ingrese el primer numero: ") )
+    b=int(input("Ingrese el segundo numero: "))
+    sum=a+b
+    print( f"El resultado de la suma es: {sum}")
+
+#sumadenumero(a,b)
+
+def ordenarunacadena(cadena):
+    cadena=str(input("Ingrese la cadena que desea ordenar: "))
+    cadenaordenada=cadena.sort()
+    print(f"La cadena ordenada en orden alfabetico es: {cadenaordenada}")
+
+ordenarunacadena(c)
+
+#def anagramas(anagrama):
